93923dba11f6139983434699d4de0a31de120247
Andrei Ignat <ignatandrei@yahoo.com>
2020-08-13 20:04:39 +0300

    Commit test with several files

---
+++ Generated/Backend/NETCore3.1/TestWebAPI_BL/Sheet1BL.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace TestWebAPI_BL
+{
+    public partial class Sheet1
+    {
+        #region partial functions
+        partial void OnConstructor();
+        partial void OnCopyConstructor(Sheet1 other, bool withID);
+        #endregion
+
+        #region constructors
+        public Sheet1 (){
+            OnConstructor();
+        }
+        
+        public Sheet1(Sheet1 other):base(){ 
+
+            OnCopyConstructor(other:other,withID: false);
+                
+        }
+        public void CopyPropertiesFrom(Sheet1 other, bool withID){
+            if(withID){
+                this.ID= other.ID;
+            }
+
+            this.asd = other.asd;
+                
+            this.basd = other.basd;
+                
+            OnCopyConstructor(other,withID);
+        }
+
+        #endregion
+        
+        #region Properties
+        public long ID{get;set;}
+            
+
+            public String asd { get; set; }
+            
+            public String basd { get; set; }
+                    #endregion
+        
+    }
+}
