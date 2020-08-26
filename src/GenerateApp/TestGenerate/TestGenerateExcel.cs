d22c07e582450cc0428eae9c51e8208122d3bc7e
Andrei Ignat <ignatandrei@yahoo.com>
2020-08-27 00:01:33 +0300

    replaceing spaces

--- src/GenerateApp/TestGenerate/TestGenerateExcel.cs
+++ src/GenerateApp/TestGenerate/TestGenerateExcel.cs
@@ -15,14 +15,14 @@ namespace TestGenerate
         }
         [Theory]
         //[InlineData(@"E:\generateApp\src\GenerateApp\GenerateApp\wwwroot\GenerateAll\ExcelTests\eu.xlsx")]
-        [InlineData(@"C:\Users\Surface1\Desktop\New folder\Daily work list Greyline 20.03.xlsx")]
+        [InlineData(@"C:\Users\ignat\Desktop\Daily work list Greyline 20.03.xlsx")]
         public async void TestData(string file)
         {
             var i = new InfoData(SourceData.Excel)
             {
                 logs = new Logs(),
                 name = "testAndrei",
-                folderGenerator = @"E:\generateApp\src\GenerateApp\GenerateApp\wwwroot\GenerateAll",
+                folderGenerator = @"E:\ignatandrei\generateApp\src\GenerateApp\GenerateApp\wwwroot\GenerateAll",
                 pathFile =  file
             };
 
