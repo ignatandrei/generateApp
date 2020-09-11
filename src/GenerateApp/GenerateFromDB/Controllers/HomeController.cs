7fe6d7347cb07c81cc884f4a18d6e86d33723dcd
ignatandrei <ignatandrei@yahoo.com>
2020-09-11 05:06:50 +0300

    returns data

--- src/GenerateApp/GenerateFromDB/Controllers/HomeController.cs
+++ src/GenerateApp/GenerateFromDB/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using GenerateApp.Controllers;
@@ -29,18 +30,18 @@ namespace GenerateFromDB.Controllers
         {
             return await payLoadConn.FromPayloadConn();
         }
-        
+
         [HttpPost]
-        public async Task<string> GenerateApp([FromBody] GenerateAppV1 app)
+        public async Task<ReturnData> GenerateApp([FromBody] GenerateAppV1 app)
         {
             await foreach (var item in app.Validate())
             {
-                throw new ArgumentException("validation error:" + item.ErrorMessage,string.Join("m", item.MemberNames));
+                throw new ArgumentException("validation error:" + item.ErrorMessage, string.Join("m", item.MemberNames));
             }
             var info = app.GenerateInfoData();
             info.folderGenerator = Path.Combine(environment.WebRootPath, "GenerateAll");
             string dateNow = DateTime.Now.ToString("yyyyMMddHHmmss");
-            info.pathFile = Path.Combine(environment.WebRootPath,dateNow,"conection.txt");
+            info.pathFile = Path.Combine(environment.WebRootPath, dateNow, "conection.txt");
             var di = Directory.CreateDirectory(Path.GetDirectoryName(info.pathFile));
             System.IO.File.WriteAllText(info.pathFile, app.payLoadConn.ConnectionString());
             var data = await info.GenerateApp();
@@ -50,9 +51,9 @@ namespace GenerateFromDB.Controllers
                 Console.WriteLine(info.logs[info.logs.Count - 1]);
                 throw new Exception(info.logs[info.logs.Count - 1]);
             }
-            
-            string pathDir =Path.GetDirectoryName (info.pathFile);
-            var powershellFile = Directory.GetFiles(pathDir, "generateWin.ps1",SearchOption.AllDirectories).FirstOrDefault();
+
+            string pathDir = Path.GetDirectoryName(info.pathFile);
+            var powershellFile = Directory.GetFiles(pathDir, "generateWin.ps1", SearchOption.AllDirectories).FirstOrDefault();
             if (powershellFile == null)
                 throw new ArgumentException("cannot find powershell docker file");
 
@@ -62,12 +63,21 @@ namespace GenerateFromDB.Controllers
             ps.Arguments = powershellFile;
             ps.CreateNoWindow = false;
             Process.Start(ps).WaitForExit();
-            string outDir = Path.Combine(Path.GetDirectoryName(powershellFile), "out", "netcore3.1", "win-x64");
 
+            string outDir = Path.Combine(Path.GetDirectoryName(powershellFile), "out", "netcore3.1", "win-x64");
             InfoData.CreateVDir(dateNow, outDir);
+
+            string generated = Path.Combine(Path.GetDirectoryName(powershellFile), "generated");
+            string zip = Path.Combine(outDir, "wwwroot", "generated.zip");
+            ZipFile.CreateFromDirectory(generated, Path.Combine(Path.GetDirectoryName(powershellFile), "generated.zip"));
+
             // execute powershell
-            return "http://alex360.go.ro:86/"+ dateNow;
-        }
+            return new ReturnData()
+            {
+                Site = dateNow,
+                ZipGenerated = "generated.zip"
+            };
+         }
         [HttpPost]
         public TableGenerator[] tableGenerator([FromBody] Table[] tables)
         {
