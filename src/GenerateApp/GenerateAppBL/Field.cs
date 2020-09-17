619d7cf261a3001d8460382617e05cf3aca80d40
ignatandrei <ignatandrei@yahoo.com>
2020-09-17 10:33:39 +0300

    field

--- src/GenerateApp/GenerateAppBL/Field.cs
+++ src/GenerateApp/GenerateAppBL/Field.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 
 namespace GenerateApp.Controllers
 {
@@ -30,15 +31,36 @@ namespace GenerateApp.Controllers
             originalType?.ToLower() switch
             {
                 null => null,
+                string s when s.Contains("bit", StringComparison.InvariantCultureIgnoreCase) => typeof(bool),
                 string s when s.Contains("bigint", StringComparison.InvariantCultureIgnoreCase) => typeof(long),
+                string s when s.Contains("decimal", StringComparison.InvariantCultureIgnoreCase) => typeof(decimal),
+                string s when s.Contains("numeric", StringComparison.InvariantCultureIgnoreCase) => typeof(double),
+                string s when s.Contains("money", StringComparison.InvariantCultureIgnoreCase) => typeof(decimal),
+                string s when s.Contains("float", StringComparison.InvariantCultureIgnoreCase) => typeof(float),
+                string s when s.Contains("real", StringComparison.InvariantCultureIgnoreCase) => typeof(decimal),
+                string s when s.Contains("time", StringComparison.InvariantCultureIgnoreCase) => typeof(DateTime),
+
+                string s when s.Contains("uniqueidentifier", StringComparison.InvariantCultureIgnoreCase) => typeof(Guid),
+
                 string s when s.Contains("int", StringComparison.InvariantCultureIgnoreCase) => typeof(int),
                 string s when s.Contains("varchar", StringComparison.InvariantCultureIgnoreCase) => typeof(string),
+                string s when s.Contains("sql_variant", StringComparison.InvariantCultureIgnoreCase) => typeof(string),
+                string s when s.Contains("char", StringComparison.InvariantCultureIgnoreCase) => typeof(string),
+                string s when s.Contains("binary", StringComparison.InvariantCultureIgnoreCase) => typeof(string),
+                string s when s.Contains("text", StringComparison.InvariantCultureIgnoreCase) => typeof(string),
+                string s when s.Contains("xml", StringComparison.InvariantCultureIgnoreCase) => typeof(string),
+                string s when s.Contains("image", StringComparison.InvariantCultureIgnoreCase) => typeof(byte[]),
+
                 string s when s.Contains("bool", StringComparison.InvariantCultureIgnoreCase) => typeof(bool),
                 string s when s.Contains("date", StringComparison.InvariantCultureIgnoreCase) => typeof(DateTime),
 
-                _ => null
+                _ => DefaultType(originalType?.ToLower())
             };
-
+        public Type DefaultType(string name)
+        {
+            System.Diagnostics.Debug.Assert(false, $"cannot find {name}");
+            return typeof(string);
+        }
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             if (string.IsNullOrWhiteSpace(name))
