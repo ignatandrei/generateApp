6dd870b90bca30f9a7e6d5efb4e9b57381e82442
ignatandrei <ignatandrei@yahoo.com>
2020-09-24 01:04:25 +0300

    finsihing number records

--- src/GenerateApp/GenerateApp/wwwroot/GenerateAll/FrontEnd/Angular10.0/src/app/dashboard/dashboard.component.ts
+++ src/GenerateApp/GenerateApp/wwwroot/GenerateAll/FrontEnd/Angular10.0/src/app/dashboard/dashboard.component.ts
@@ -8,21 +8,23 @@
   string ClassNameFromTableName(string tableName){
 		return tableName.Replace(" ","").Replace(".","").Replace("(","").Replace(")","");
   }
-  string services = "";
+  string services = "private titleService: Title, private metaService: Meta   ";
   for (int iRowDS = 0; iRowDS < nrRowsDS; iRowDS++)
   {
     var nameTable = ds.Rows[iRowDS]["TableName"].ToString();
-    services+= ClassNameFromTableName(nameTable).ToLower() +":"+ ClassNameFromTableName(nameTable)+"Service";
+    services+= " , private "+ClassNameFromTableName(nameTable).ToLower() +"Service:"+ ClassNameFromTableName(nameTable)+"Service";
 
   }
 
 }
+import { Title, Meta } from '@(angular)/platform-browser';
 import { Component, OnInit } from '@(angular)/core';
 @for (int iRowDS = 0; iRowDS < nrRowsDS; iRowDS++)
 {
   var nameTable = ds.Rows[iRowDS]["TableName"].ToString();
+  var className=ClassNameFromTableName(nameTable);
 <text>
-import { @(nameTable)Service } from '../services/@(nameTable).service';
+import { @(className)Service } from '../services/@(className).service';
 </text>
 
 }
@@ -36,16 +38,36 @@ export class DashboardComponent implements OnInit {
   @for (int iRowDS = 0; iRowDS < nrRowsDS; iRowDS++)
   {
     var nameTable = ds.Rows[iRowDS]["TableName"].ToString();
+    var className=ClassNameFromTableName(nameTable);
   <text>
-  public @("numberRecords"+ nameTable): number;
+  public @("numberRecords"+ className.ToLower()): number;
   </text>
   
   }
   
-  constructor(private dep: dboDepartmentService) { }
+  constructor(@(services)) {
+       this.titleService.setTitle('Main dashboard');
+      this.metaService.addTags([
+        {name: 'keywords', content: 'dashboard'},
+        {name: 'description', content: 'dashboard'},
+        {name: 'robots', content: 'index, follow'}
+      ]);
+
+   }
 
   ngOnInit(): void {
-    this.dep.Count().subscribe(it=> this.numberRecordsDep=it);
+
+    @for (int iRowDS = 0; iRowDS < nrRowsDS; iRowDS++)
+    {
+      var nameTable = ds.Rows[iRowDS]["TableName"].ToString();
+      var className=ClassNameFromTableName(nameTable);
+      <text>
+      this.@(className.ToLower())Service.Count().subscribe(it=> this.numberRecords@(className.ToLower())=it);      
+      </text>
+
+    }  
+
+    
   }
 
 }
