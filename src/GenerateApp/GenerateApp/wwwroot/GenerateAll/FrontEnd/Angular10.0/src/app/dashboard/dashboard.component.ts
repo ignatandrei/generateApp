4f55b1b0ddc884df837250f3e8f44ac93cacdc57
ignatandrei <ignatandrei@yahoo.com>
2020-09-24 00:29:51 +0300

    generate count test

--- src/GenerateApp/GenerateApp/wwwroot/GenerateAll/FrontEnd/Angular10.0/src/app/dashboard/dashboard.component.ts
+++ src/GenerateApp/GenerateApp/wwwroot/GenerateAll/FrontEnd/Angular10.0/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,31 @@
+@model Stankins.Interfaces.IDataToSent
 @{
 	var Component = "@Component";
-	var angular="@angular";
+  var angular="@angular";
+  var ds= Model.FindAfterName("DataSource").Value;    
+  var nrRowsDS=ds.Rows.Count;
+
+  string ClassNameFromTableName(string tableName){
+		return tableName.Replace(" ","").Replace(".","").Replace("(","").Replace(")","");
+  }
+  string services = "";
+  for (int iRowDS = 0; iRowDS < nrRowsDS; iRowDS++)
+  {
+    var nameTable = ds.Rows[iRowDS]["TableName"].ToString();
+    services+= ClassNameFromTableName(nameTable).ToLower() +":"+ ClassNameFromTableName(nameTable)+"Service";
+
+  }
+
 }
 import { Component, OnInit } from '@(angular)/core';
+@for (int iRowDS = 0; iRowDS < nrRowsDS; iRowDS++)
+{
+  var nameTable = ds.Rows[iRowDS]["TableName"].ToString();
+<text>
+import { @(nameTable)Service } from '../services/@(nameTable).service';
+</text>
 
+}
 @(Component)({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -11,7 +33,15 @@ import { Component, OnInit } from '@(angular)/core';
 })
 export class DashboardComponent implements OnInit {
 
-  public numberRecordsDep: number;
+  @for (int iRowDS = 0; iRowDS < nrRowsDS; iRowDS++)
+  {
+    var nameTable = ds.Rows[iRowDS]["TableName"].ToString();
+  <text>
+  public @("numberRecords"+ nameTable): number;
+  </text>
+  
+  }
+  
   constructor(private dep: dboDepartmentService) { }
 
   ngOnInit(): void {
