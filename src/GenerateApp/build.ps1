3bd1480afafb6bc6fb9b0420d48774ac6d14b6ff
ignatandrei <ignatandrei@yahoo.com>
2020-10-06 22:49:27 +0300

    add item

--- src/GenerateApp/build.ps1
+++ src/GenerateApp/build.ps1
@@ -5,6 +5,7 @@ dotnet tool list
 #dotnet pack  --include-source --include-symbols --output ../output/
 #c
 cd ./GenerateAppTool
+Remove-Item ./GenerateAll/GenerateAll.zip 
 Write-Host "start copy"
 Compress-Archive -Path ../GenerateApp/wwwroot/GenerateAll/* -DestinationPath ./GenerateAll/GenerateAll.zip 
 Write-Host "end copy"
