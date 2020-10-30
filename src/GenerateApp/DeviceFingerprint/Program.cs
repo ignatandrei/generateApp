803b719c7d651f5048428791f0bb889aa10b3d9a
Alexandru Badita <alexandru360@gmail.com>
2020-10-30 14:31:08 +0200

    unique machine fingerprint and program ID

---
+++ src/GenerateApp/DeviceFingerprint/Program.cs
@@ -0,0 +1,37 @@
+﻿using DeviceId;
+using DeviceId.Encoders;
+using DeviceId.Formatters;
+using System;
+using System.Security.Cryptography;
+
+namespace DeviceFingerprint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string deviceId = new DeviceIdBuilder()
+                .AddProcessorId()
+                .AddMotherboardSerialNumber()
+                .UseFormatter(new HashDeviceIdFormatter(() => SHA256.Create(), new Base64UrlByteArrayEncoder()))
+                .ToString().ToUpper();
+            deviceId = StringUtils.FormatLikeGUID(deviceId.Replace("-",""), 3);
+
+            Console.WriteLine("DeviceID: " + deviceId);
+
+            var fingerprintUtilsID = FingerprintUtils.Value();
+
+            Console.WriteLine("Fingerprint Utils ID: " + fingerprintUtilsID);
+
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("===================================================================================");
+            Console.WriteLine("==========================To be used in production !!==============================");
+            Console.WriteLine("Program and client unique ID: " + deviceId + "-" +fingerprintUtilsID);
+            Console.WriteLine("===================================================================================");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/GenerateApp/DeviceFingerprint/StringUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace DeviceFingerprint
+{
+    public static class StringUtils
+    {
+        public static string FormatLikeGUID(string input, double splitSize)
+        {
+            int k = 0;
+            var output = input.ToLookup(c => Math.Floor(k++ / splitSize))
+                .Select(e => new String(e.ToArray())).ToArray();
+
+            return string.Join("-", output);
+        }
+    }
+}
