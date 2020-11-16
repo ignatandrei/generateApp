81376c78c5aafc8581283f60b0c05ca7fa4ab1a4
ignatandrei <ignatandrei@yahoo.com>
2020-11-16 02:14:00 +0200

    now it works!

--- src/GenerateApp/GenerateAppWPF/App.xaml.cs
+++ src/GenerateApp/GenerateAppWPF/App.xaml.cs
@@ -5,6 +5,11 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Diagnostics;
+using System.IO.Compression;
 
 namespace GenerateAppWPF
 {
@@ -13,9 +18,37 @@ namespace GenerateAppWPF
     /// </summary>
     public partial class App : Application
     {
+        private Process process;
         public App()
         {
+            Console.WriteLine("start");
+            ZipFile.ExtractToDirectory("GenerateFromDB.zip", ".", true);
+            //string applicationDirectoryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            //string file = Path.Combine(applicationDirectoryPath, "SNI.dll");
+            //if (!File.Exists(file))
+            //{                
+            //    using (WebClient client = new WebClient())
+            //    {
+            //        client.DownloadFile("https://data-to-code.eu/app/SNI.dll.deploy", file);
+            //    }
+            //}
+            //file = Path.Combine(applicationDirectoryPath, "Microsoft.Data.SqlClient.SNI.dll");
+            //if (!File.Exists(file))
+            //{
+                
+            //    using (WebClient client = new WebClient())
+            //    {
+            //        client.DownloadFile("https://data-to-code.eu/app/Microsoft.Data.SqlClient.SNI.dll.deploy", file);
+            //    }
+            //}
             this.Startup += App_Startup;
+            this.Exit += App_Exit;
+        }
+
+        private void App_Exit(object sender, ExitEventArgs e)
+        {
+            if (!process.HasExited)
+                process.Kill(true);
         }
 
         private void App_Startup(object sender, StartupEventArgs e)
@@ -26,7 +59,12 @@ namespace GenerateAppWPF
             {
                  try
                  {
-                     GenerateFromDB.Program.Main(e.Args);
+                    var dir = Path.Combine(Environment.CurrentDirectory, "publish");
+                    var psi = new ProcessStartInfo(Path.Combine(dir, "GenerateFromDb.exe"));
+                    psi.WorkingDirectory = dir;
+                    psi.CreateNoWindow = true;
+                    process = Process.Start(psi);
+                     //GenerateFromDB.Program.Main(e.Args);
                  }
                  catch (Exception ex)
                  {
